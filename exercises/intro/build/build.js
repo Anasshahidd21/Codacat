1d24fbd5ce47a7b69f5b4aab1031d37a2557077d
halhex <dev@halhex.ca>
2020-02-01 17:45:56 -0500

    responsive cat

--- exercises/intro/build/build.js
+++ exercises/intro/build/build.js
@@ -22,42 +22,55 @@ var Cat = (function () {
     Cat.prototype.draw = function () {
         noStroke();
         fill('red');
-        ellipse(this.location.x, this.location.y, 300);
-        this.drawEye(createVector(this.location.x - 60, this.location.y - 50));
-        this.drawEye(createVector(this.location.x + 60, this.location.y - 50));
+        imageMode('center');
+        var h = width;
+        var w = 0.9116684841875682 * h;
+        image(catImage, this.location.x, this.location.y, w, h);
+        this.drawEye(createVector(this.location.x - width * 0.14, this.location.y + width * 0.0625));
+        this.drawEye(createVector(this.location.x + width * 0.14, this.location.y + width * 0.0625));
     };
     Cat.prototype.drawEye = function (location) {
-        if (this.spooked)
-            fill('black');
-        else
-            fill('white');
-        ellipse(location.x, location.y, 50);
         if (this.lookingAt) {
             var t = p5.Vector.sub(this.lookingAt, location)
                 .mult(.15)
-                .limit(15);
+                .limit(width * 0.025);
             location.add(t);
         }
+        this.drawupil(location);
+    };
+    Cat.prototype.drawupil = function (location) {
+        var ratio = 0.9852941176470589;
+        var h = width * .1, w = h * ratio;
         fill('black');
-        ellipse(location.x, location.y, 15, 20);
+        image(pupilImage, location.x, location.y, w, h);
     };
     return Cat;
 }());
 var cat;
+var catImage;
+var pupilImage;
+function preload() {
+    catImage = loadImage('../assets/eyecat.png');
+    pupilImage = loadImage('../assets/pupil.png');
+}
 function setup() {
     var canvas = createCanvas(800, 800);
     if (document.getElementById('canvas'))
         canvas.parent('canvas');
-    cat = new Cat(createVector(width / 2, height / 2));
+    cat = new Cat(createVector(width / 2, height - width * 0.3125));
+}
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
 function draw() {
     background('white');
     cat.reset();
     main();
     var speed = 5;
+    cat.location.x = width / 2;
     cat.location.y = cat.isHidden
-        ? Math.min(height + 100, cat.location.y + speed)
-        : Math.max(height / 2, cat.location.y - speed);
+        ? Math.min(height - height * 0.3125, cat.location.y + speed)
+        : Math.max(height * 0.4375, cat.location.y - speed);
     cat.draw();
 }
 function main() {
--- exercises/intro/sketch/sketch.ts
+++ exercises/intro/sketch/sketch.ts
@@ -1,5 +1,13 @@
 let cat: Cat;
 
+let catImage: p5.Image;
+let pupilImage: p5.Image;
+
+function preload() {
+    catImage = loadImage('../assets/eyecat.png');
+    pupilImage = loadImage('../assets/pupil.png');
+}
+
 function setup() {
     // Create canvas
     let canvas = createCanvas(800, 800);
@@ -7,10 +15,14 @@ function setup() {
 
     // Intialize universe
     cat = new Cat(
-        createVector(width / 2, height / 2)
+        createVector(width / 2, height - width * 0.3125)
     );
 }
 
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+  
 function draw() {
     background('white');
 
@@ -18,9 +30,10 @@ function draw() {
     main();
 
     const speed = 5;
+    cat.location.x = width / 2;
     cat.location.y = cat.isHidden
-        ? Math.min(height + 100, cat.location.y + speed)
-        : Math.max(height / 2, cat.location.y - speed);
+        ? Math.min(height - height * 0.3125, cat.location.y + speed)
+        : Math.max(height * 0.4375, cat.location.y - speed);
 
     cat.draw();
 }
